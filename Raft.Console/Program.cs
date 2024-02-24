973dc1d32c10339e3483f89c2361a2189dc1d294
anthonyhardman <anthonythardman@gmail.com>
2024-02-24 14:12:03 -0700

    election

--- Raft.Console/Program.cs
+++ Raft.Console/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raft.Console;
+
+var threads = new List<Thread>();
+
+for (int i = 0; i < 5; i++)
+{
+    var node = new Node();
+    var thread = new Thread(node.Run);
+    Console.WriteLine($"Node {node.Id} started as {node.Role}");
+    thread.Start();
+    threads.Add(thread);
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+Node.StopAll();
+
+foreach (var thread in threads)
+{
+    thread.Join();
+}
